8be08cc4a4ef3d4a0d54197d3257d63ef2de58d2
riddhiRaina <riddhiraina54@gmail.com>
2025-10-05 23:51:06 -0400

    Final version for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(s9^pcv)gtxi3ojl8#*onw@+))le2e1c(pojtbhnzxs_hp*e$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
